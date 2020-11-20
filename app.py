06dd7ce81c4866864e2697cab272a46e8d67cd90
Alissa Troiano <alissatroianonyc@gmail.com>
2020-11-20 17:43:08 +0000

    Add user profile page with functionality

--- app.py
+++ app.py
@@ -42,8 +42,10 @@ def signup():
         }
         mongo.db.users.insert_one(signup)
 
+        # create session cookies
         session["user"] = request.form.get("username").lower()
         flash("Success!")
+        return redirect(url_for("profile", username=session["user"]))
     return render_template("signup.html")
 
 
@@ -57,7 +59,10 @@ def login():
             if check_password_hash(
                 existing_user["password"], request.form.get("password")):
                     session["user"] = request.form.get("username").lower()
-                    flash("Welcome, {}".format(request.form.get("username")))
+                    flash("Welcome, {}".format(
+                        request.form.get("username")))
+                    return redirect(url_for(
+                        "profile", username=session["user"]))
             else:
                 flash("Incorrect username and/or password")
                 return redirect(url_for("login"))
@@ -67,6 +72,14 @@ def login():
             return redirect(url_for("login"))
 
     return render_template("login.html")
+
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    # Fetch session username from databases
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    return render_template("profile.html", username=username)
 
 
 if __name__ == "__main__":
