c37b63dfd875a1ab79cb6ddbdacaf531401378b0
Alissa Troiano <alissatroianonyc@gmail.com>
2020-11-22 05:06:46 +0000

    Create add recipe template and apply custom styles

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ mongo = PyMongo(app)
 @app.route("/")
 @app.route("/get_recipes")
 def get_recipes():
-    recipes = list(mongo.db.recipes.find())
+    recipes = mongo.db.recipes.find()
     return render_template("recipes.html", recipes=recipes)
 
 
@@ -93,6 +93,12 @@ def logout():
     flash("You have been logged out")
     session.pop("user")
     return redirect(url_for("login"))
+
+
+@app.route("/add_recipe")
+def add_recipe():
+    # Add Recipe Functionality
+    return render_template("add_recipe.html")
 
 
 if __name__ == "__main__":
