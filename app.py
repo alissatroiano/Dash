4dc1f86c8f5c63fe529fc9dc7192371110ddd233
Alissa Troiano <alissatroianonyc@gmail.com>
2020-12-09 18:35:16 +0000

    Refactor upload function to return url for uploading image

--- app.py
+++ app.py
@@ -151,7 +151,9 @@ def upload_file():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            path = url_for('static', filename='uploads/'+filename)
+            image = url_for('static', filename='uploads/'+filename)
+            return image
+        else:
             return path
 
 
