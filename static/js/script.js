e52d602f0f8a788890eb8b7957ec521e050acce1
Alissa Troiano <alissatroianonyc@gmail.com>
2020-12-11 01:12:51 +0000

    Remove whitespace and beautify

--- static/js/script.js
+++ static/js/script.js
@@ -4,4 +4,5 @@ $(document).ready(function() {
     $('.sidenav').sidenav({edge: "right"});
     $('.fixed-action-btn').floatingActionButton();
     $('.waves-effect.waves-light').floatingActionButton();
+    $('.tooltipped').tooltip();
 });
