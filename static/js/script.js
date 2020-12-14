a602696e6576abb8aff2ccd60e7ee3df97a728db
Alissa Troiano <alissatroianonyc@gmail.com>
2020-12-14 17:21:07 +0000

    Add comment with credits to script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,5 @@
- // jQuery
+ // jQuery Copied from <https://materializecss.com/>
+
  $(document).ready(function() {
      $('.sidenav').sidenav({
          edge: "right"
@@ -6,26 +7,4 @@
      $('.fixed-action-btn').floatingActionButton();
      $('.waves-effect.waves-light').floatingActionButton();
      $('.tooltipped').tooltip();
- });
-
- $(function() {
-     $('#delete-recipe').click(function() {
-         let confirm = confirm("Are you sure?");
-         if (confirm == true) {
-             $.ajax({
-                 url: '/recipes/{{row[0]}}',
-
-                 success: function(response) {
-                     console.log(response);
-                 },
-                 error: function(error) {
-                     console.log(error);
-                 }
-             });
-
-         } else {
-
-         }
-     });
-
  });
