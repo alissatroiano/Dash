1ac3f9f5222955aa6c8fdb748b1654cd0997f707
Alissa Troiano <alissatroianonyc@gmail.com>
2020-12-17 02:56:11 +0000

    Refactor to pass

--- test_app.py
+++ test_app.py
@@ -2,23 +2,22 @@ import app
 
 
 def test_search_results_empty():
-    # If the test_query string = search index the test fails.
-    query = 'Test Recipe'
-    get_recipes_count = app.mongo.db.recipes.count_documents(
+    query = 'Hakuna Matata'
+    search_count = app.mongo.db.recipes.count_documents(
         {'$text': {'$search': query}})
 
-    if get_recipes_count < 1:
-        get_recipes = 'Empty'
+    if search_count < 1:
+        search = 'Empty'
     else:
-        get_recipes_count = app.mongo.db.recipes.find(
+        search = app.mongo.db.recipes.find(
             {"$text": {"$search": query}})
-    assert get_recipes == 'Empty'
+    assert search == 'Empty'
 
 
 def test_add_recipe():
     recipes = app.mongo.db.recipes
     recipes.insert_one({
-        'recipe_name': 'Test Recipe',
+        'recipe_name': 'Hakuna Matata',
         'recipe_description': 'Hello pytest',
         'prep_time': '2 hours',
         'tools_needed': 'keyboard, laptop, pytest',
@@ -27,3 +26,16 @@ def test_add_recipe():
     recipe_query = app.mongo.db.recipes.find_one(
         {'recipe_name': 'Test Recipe'})
     assert recipe_query['recipe_name'] == 'Test Recipe'
+
+
+def test_search_results_retest():
+    # test search again. Should not be empty after adding new recipe above.
+    query = "Hakuna Matata"
+    search_count = app.mongo.db.recipes.count_documents(
+        {'$text': {'$search': query}})
+    if search_count < 1:
+        search = 'Empty'
+    else:
+        search = app.mongo.db.recipes.find(
+            {'$text': {'$search': query}})
+    assert search != 'Empty'
